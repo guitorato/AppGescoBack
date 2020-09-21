2267bd20f8a961201ee84f23ba4a5c7de9ac427a
Guilherme Torato <guii.torato@gmail.com>
2020-09-21 13:34:22 -0300

    Adicionado a classe Hospital

---
+++ src/main/java/com/gesco/domain/Hospital.java
@@ -0,0 +1,39 @@
+package com.gesco.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Hospital implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String endereco;
+	
+	@Column(nullable = false)
+	private String numero;
+	
+	@Column(nullable = false)
+	private String bairro;
+	
+	@Column(nullable = false)
+	private String cidade;
+	
+	@OneToMany(mappedBy="hospital")
+	private List<Funcionario> funcionarios; 
+
+}
