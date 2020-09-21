4a3cc0e04ed234981309b6cd5ddfe69b44340a72
Guilherme Torato <guii.torato@gmail.com>
2020-09-21 00:54:26 -0300

    Criado o metodo de busca por id (findId)

--- src/main/java/com/gesco/resources/FuncionarioResource.java
+++ src/main/java/com/gesco/resources/FuncionarioResource.java
@@ -1,12 +1,29 @@
 package com.gesco.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gesco.domain.Funcionario;
+import com.gesco.services.FuncionarioService;
 
 @RestController
 @RequestMapping(value="/funcionarios")
 public class FuncionarioResource {
 	
+	@Autowired
+	private FuncionarioService service;
 	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> findId(@PathVariable Integer id){
+		
+		Funcionario obj = service.find(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
 }
