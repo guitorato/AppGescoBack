c374509a60207612d29d45206bc22a1bfe987462
Guilherme Torato <guii.torato@gmail.com>
2020-09-21 13:41:10 -0300

    Modificações na classe Hospital

--- src/main/java/com/gesco/domain/Hospital.java
+++ src/main/java/com/gesco/domain/Hospital.java
@@ -34,6 +34,115 @@ public class Hospital implements Serializable {
 	private String cidade;
 	
 	@OneToMany(mappedBy="hospital")
-	private List<Funcionario> funcionarios; 
+	private List<Funcionario> funcionarios;
+
+	public Hospital() {}
+	
+	public Hospital(Integer id, String nome, String endereco, String numero, String bairro, String cidade,
+			List<Funcionario> funcionarios) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.funcionarios = funcionarios;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Hospital [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", numero=" + numero + ", bairro="
+				+ bairro + ", cidade=" + cidade + ", funcionarios=" + funcionarios + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hospital other = (Hospital) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 
 }
