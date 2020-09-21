2096bab967112a6e29d0403f61c8f3d4ee289bc8
Guilherme Torato <guii.torato@gmail.com>
2020-09-20 21:44:32 -0300

    Estrutura inicial do back

---
+++ src/main/java/com/gesco/GescoApplication.java
@@ -0,0 +1,13 @@
+package com.gesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GescoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gesco/GescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
