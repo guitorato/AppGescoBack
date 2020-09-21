549f68ae815bb3b08a5e6778f8b849eecfd18350
Guilherme Torato <guii.torato@gmail.com>
2020-09-20 22:37:30 -0300

    Criado a classe Funcionario e a classe de chamada do Rest

---
+++ src/main/java/com/gesco/domain/Funcionario.java
@@ -0,0 +1,133 @@
+package com.gesco.domain;
+
+import java.io.Serializable;
+
+import javax.xml.crypto.Data;
+
+public class Funcionario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Data dtNascimento;
+	private boolean sexo;
+	private String nameUser;
+	private Integer tipoFunc;
+	private String conselho;
+	private String cdConselho;
+	
+	public Funcionario () {}
+
+	public Funcionario(Integer id, String nome, Data dtNascimento, boolean sexo, String nameUser,
+			Integer tipoFunc, String conselho, String cdConselho) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dtNascimento = dtNascimento;
+		this.sexo = sexo;
+		this.nameUser = nameUser;
+		this.tipoFunc = tipoFunc;
+		this.conselho = conselho;
+		this.cdConselho = cdConselho;
+	}
+
+	public Integer getIdHospital() {
+		return id;
+	}
+
+	public void setIdHospital(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Data getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Data dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public boolean isSexo() {
+		return sexo;
+	}
+
+	public void setSexo(boolean sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getNameUser() {
+		return nameUser;
+	}
+
+	public void setNameUser(String nameUser) {
+		this.nameUser = nameUser;
+	}
+
+	public Integer getTipoFunc() {
+		return tipoFunc;
+	}
+
+	public void setTipoFunc(Integer tipoFunc) {
+		this.tipoFunc = tipoFunc;
+	}
+
+	public String getConselho() {
+		return conselho;
+	}
+
+	public void setConselho(String conselho) {
+		this.conselho = conselho;
+	}
+
+	public String getCdConselho() {
+		return cdConselho;
+	}
+
+	public void setCdConselho(String cdConselho) {
+		this.cdConselho = cdConselho;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + ", dtNascimento=" + dtNascimento + ", sexo="
+				+ sexo + ", nameUser=" + nameUser + ", tipoFunc=" + tipoFunc + ", conselho=" + conselho
+				+ ", cdConselho=" + cdConselho + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/gesco/resources/FuncionarioResource.java
@@ -0,0 +1,12 @@
+package com.gesco.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/funcionarios")
+public class FuncionarioResource {
+	
+	
+}
