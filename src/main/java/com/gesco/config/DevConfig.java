c70fc5405ebac9316a6576f1824c00aee0d6288b
Guilherme Torato <guii.torato@gmail.com>
2020-10-01 19:35:35 -0300

    Criando o profile Dev com MySQL

---
+++ src/main/java/com/gesco/config/DevConfig.java
@@ -0,0 +1,16 @@
+package com.gesco.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Bean
+	public boolean instantiateDatabase() {
+		
+		return true;
+	}
+}
