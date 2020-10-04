9406681e03ed711bba8d1d53b38a75a071f8a19b
Guilherme Torato <guii.torato@gmail.com>
2020-10-03 22:56:05 -0300

    Criado uma classe enums para definição do cargo do funcionario (Médico,Farmaceutico,Internação)

---
+++ src/main/java/com/gesco/domain/enums/TipoFuncionario.java
@@ -0,0 +1,39 @@
+package com.gesco.domain.enums;
+
+public enum TipoFuncionario {
+
+	MEDICO(1,"MÉDICO"),
+	FARMACEUTICO(2,"Farmacêutico"),
+	INTERNACAO(3,"Internação");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoFuncionario(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoFuncionario toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for(TipoFuncionario x : TipoFuncionario.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Acesso inválido");
+	}
+	
+}
