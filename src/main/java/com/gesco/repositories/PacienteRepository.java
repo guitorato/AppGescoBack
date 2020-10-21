9781f20ba2e64b34a647ce6e6bbb477c1e318aa5
Guilherme Torato <guii.torato@gmail.com>
2020-10-21 18:04:05 -0300

    Adc anotação @Query paa busca por nome dos pacientes

--- src/main/java/com/gesco/repositories/PacienteRepository.java
+++ src/main/java/com/gesco/repositories/PacienteRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,15 +14,15 @@ import com.gesco.domain.Paciente;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
 
-	 @Transactional(readOnly=true)
+	@Query("SELECT p FROM Paciente p WHERE p.nome LIKE %:nome% ORDER BY registry ASC")
 	 List<Paciente> findByNome(String nome);
 	 
 	 
-	 @Transactional(readOnly=true)
-	 List<Paciente> findByRegistry(Long registry);
+	//@Query("SELECT p FROM Paciente p WHERE p.registry LIKE %:registry% ORDER BY `registry` ASC")
+	 //List<Paciente> findByRegistry(Long registry);
 	 
-	 @Transactional(readOnly=true)
-	 List<Paciente> findByNomeAndRegistry(String nome,Long registry);
+	 //@Query("SELECT p FROM Paciente p WHERE p.nome LIKE %:nome% AND p.registry LIKE %:registry% ")
+	 //List<Paciente> findByNomeAndRegistry(String nome,Long registry);
 	 
 	 
 	
