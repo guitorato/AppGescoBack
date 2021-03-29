4169784aaeed95a28fcab1b7b900cae42d5e0839
Guilherme Torato <guii.torato@gmail.com>
2021-03-29 20:35:43 -0300

    Instanciando dados para o paciente, para teste

--- src/main/java/com/gesco/services/DBService.java
+++ src/main/java/com/gesco/services/DBService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gesco.domain.Hospital;
+import com.gesco.domain.Paciente;
 import com.gesco.repositories.FuncionarioRepository;
 import com.gesco.repositories.HospitalRepository;
+import com.gesco.repositories.PacienteRepository;
 
 
 @Service
@@ -19,14 +21,29 @@ public class DBService {
 	private FuncionarioRepository funcionarioRepository;
 	@Autowired
 	private HospitalRepository hospitalRepository;
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
 	
 	
 	public void instantiateTestDataBase() throws ParseException {
 		
 		
 		Hospital hosp1 = new Hospital("SPDM", "Hospital de Praia Grande - SP");
+		Hospital hosp2 = new Hospital("UPA-Quietude", "Pronto Socorro Quietude");
 		
 		hospitalRepository.save(hosp1);
+		hospitalRepository.save(hosp2);
+		
+		Paciente pac1 = new Paciente("Guilherme", 3488759, LocalDate.now(), false, true, hosp1);
+		Paciente pac2 = new Paciente("Matheus", 3555759, LocalDate.now(), false, true, hosp1);
+		Paciente pac3 = new Paciente("Marcelo", 3666759, LocalDate.now(), false, false, hosp1);
+		Paciente pac4 = new Paciente("Carlos", 3777759, LocalDate.now(), false, false, hosp1);
+		
+		pacienteRepository.save(pac1);
+		pacienteRepository.save(pac2);
+		pacienteRepository.save(pac3);
+		pacienteRepository.save(pac4);
 		
 	}
 
