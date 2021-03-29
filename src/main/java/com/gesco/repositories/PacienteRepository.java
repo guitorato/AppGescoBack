f730e6757ec8f23a41a69340e6744cbd09243315
Guilherme Torato <guii.torato@gmail.com>
2021-03-29 20:31:51 -0300

    Adc a Classe PacienteResource

---
+++ src/main/java/com/gesco/repositories/PacienteRepository.java
@@ -0,0 +1,33 @@
+package com.gesco.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.gesco.domain.Paciente;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+
+	@Query("SELECT p FROM Paciente p WHERE p.nome LIKE %:nome% AND p.statusPaciente = true")
+	 List<Paciente> findByNome(String nome);
+	
+	@Query("SELECT p FROM Paciente p WHERE p.registro = :registro AND p.statusPaciente = true")
+	List<Paciente> findByRegistro(Integer registro);
+	 
+	Optional<Paciente> findByRegistroLike (Integer registro);
+	
+	@Transactional
+	Paciente deleteByRegistro (Integer registro);
+	
+	List<Paciente> findByStatusPaciente (boolean statusTratamento);
+	 
+	 
+	
+
+}
---
+++ src/main/java/com/gesco/resources/PacienteResource.java
@@ -0,0 +1,89 @@
+package com.gesco.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gesco.domain.Paciente;
+import com.gesco.dto.PacienteDTO;
+import com.gesco.helpers.Helper;
+import com.gesco.repositories.PacienteRepository;
+import com.gesco.services.PacienteService;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value="api/paciente")
+public class PacienteResource {
+	
+	@Autowired
+	private PacienteService service;
+	
+	@Autowired
+	private PacienteRepository repo;
+	
+	@ApiOperation(value = "BUSCA POR NOME DO PACIENTE OU POR REGISTRO, DE PACIENTES INTERNADOS")
+	@RequestMapping(value="/{param}", method=RequestMethod.GET , produces="application/json")
+	public ResponseEntity<List<PacienteDTO>> findName(@PathVariable String param ){
+
+		List<Paciente> list = Helper.isNumber(param) ? 
+				service.findRegistro(Integer.parseInt(param)) : 
+					service.findNome(param);
+		List<PacienteDTO> listDto = list.stream().map(obj -> new PacienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@ApiOperation(value = "LISTAR TODOS OS PACIENTES DE ACORDO COM STATUS INTERNADO(TRUE) E ALTA(FALSE)")
+	@RequestMapping(method=RequestMethod.GET , produces="application/json")
+	public ResponseEntity<List<PacienteDTO>> findStatusPaciente(@RequestParam boolean status){
+		
+		List<Paciente> list = service.findStatusTratamento(status);
+	    List<PacienteDTO> listDto = list.stream().map(obj -> new PacienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	
+	@ApiOperation(value = "DELETA TODOS OS PACIENTES")
+	@RequestMapping(value="/{registro}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer registro){
+		
+		service.delete(registro);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@ApiOperation(value = "ATUALIZA TODOS OS PACIENTES")
+	@RequestMapping(value="/{registro}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody Paciente obj , @PathVariable Integer registro){
+		obj.setId(repo.findByRegistroLike(registro).get().getId());
+		obj.setRegistro(registro);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@ApiOperation(value = "INSERE UM PACIENTE")
+	@RequestMapping(method = RequestMethod.POST )
+	public ResponseEntity<Void> insert(@Valid @RequestBody Paciente obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").build(obj.getId());
+		return ResponseEntity.created(uri).build();
+	}
+
+}
