efa4aaa349d298421144d7a99bb476786936e964
Guilherme Torato <guii.torato@gmail.com>
2021-03-30 19:42:45 -0300

    Criado a Classe DTO para antibiótico

---
+++ src/main/java/com/gesco/dto/AntibioticoDTO.java
@@ -0,0 +1,77 @@
+package com.gesco.dto;
+
+import java.time.LocalDate;
+
+import com.gesco.domain.Antibiotico;
+
+public class AntibioticoDTO {
+	
+	private String nome;
+	private String nomeComercial;
+	private LocalDate dataValidade;
+	private String lote;
+	private String tipoAplicacao;
+	private String funcionario;
+	
+	public AntibioticoDTO() {}
+	public AntibioticoDTO(Antibiotico obj) {
+		this.nome = obj.getNome();
+		this.nomeComercial = obj.getNomeComercial();
+		this.dataValidade = obj.getDataValidade();
+		this.lote = obj.getLote();
+		this.tipoAplicacao = obj.getTipoAplicacao().getDescricao();
+		this.funcionario = obj.getFuncionario().getNome();
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeComercial() {
+		return nomeComercial;
+	}
+
+	public void setNomeComercial(String nomeComercial) {
+		this.nomeComercial = nomeComercial;
+	}
+
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(LocalDate dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	public String getLote() {
+		return lote;
+	}
+
+	public void setLote(String lote) {
+		this.lote = lote;
+	}
+
+	public String getTipoAplicacao() {
+		return tipoAplicacao;
+	}
+
+	public void setTipoAplicacao(String tipoAplicacao) {
+		this.tipoAplicacao = tipoAplicacao;
+	}
+
+	public String getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(String funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	
+	
+}
