52098a5dc10d1e10e879c711e63d10d69f258882
Guilherme Torato <guii.torato@gmail.com>
2021-03-29 23:08:39 -0300

    Adc  NoSuchElementException no pacote de service.exception

---
+++ src/main/java/com/gesco/services/exceptions/NoSuchElementException.java
@@ -0,0 +1,16 @@
+package com.gesco.services.exceptions;
+
+public class NoSuchElementException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public NoSuchElementException (String msg) {
+		super(msg);
+	}
+	
+	public NoSuchElementException (String msg, Throwable cause) {
+		super(msg,cause);
+	}
+
+}
