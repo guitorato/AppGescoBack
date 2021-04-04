82407042d50708bcbcfd39977e98022f68d084c5
Guilherme Torato <guii.torato@gmail.com>
2021-04-04 20:18:42 -0300

    Criado Query para buscar por nome do paciente e por registro

--- src/main/java/com/gesco/repositories/TratamentoRepository.java
+++ src/main/java/com/gesco/repositories/TratamentoRepository.java
@@ -11,12 +11,21 @@ import org.springframework.stereotype.Repository;
 import com.gesco.domain.Antibiotico;
 import com.gesco.domain.Paciente;
 import com.gesco.domain.Tratamento;
+import com.gesco.dto.PrescricaoDTO;
 
 @Repository
 public interface TratamentoRepository extends JpaRepository<Tratamento, Integer> {
 	
+	@Query("SELECT t FROM Tratamento t, Paciente p WHERE t.paciente=p.id AND p.nome LIKE %:nome%")
+	List<Tratamento> findByNome (String nome);
 	
 	@Query("SELECT t FROM Tratamento t WHERE t.paciente = :paciente")
 	Optional<Tratamento> findPaciente (Integer paciente);
+	
+	@Query("SELECT t FROM Tratamento t, Paciente p WHERE t.paciente=p.id AND p.registro = :registro")
+	Optional<Tratamento> findRegistroPaciente (Integer registro);
+	
+	@Query("SELECT t FROM Tratamento t WHERE t.prescricoes = :prescricoes")
+	List<Tratamento> findByPrescricao(Integer prescricoes);
 
 }
