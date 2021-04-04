09684faf4a7f2699f2b97f2118940586f0d6a33f
Guilherme Torato <guii.torato@gmail.com>
2021-04-03 23:14:42 -0300

    Adc Query de update do StatusTratamento + Descrição

--- src/main/java/com/gesco/repositories/PrescricaoRepository.java
+++ src/main/java/com/gesco/repositories/PrescricaoRepository.java
@@ -3,8 +3,12 @@ package com.gesco.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.gesco.domain.Antibiotico;
@@ -14,6 +18,12 @@ import com.gesco.domain.Tratamento;
 @Repository
 public interface PrescricaoRepository extends JpaRepository<Prescricao, Integer> {
 	
+	@Transactional
+	@Modifying
+	@Query("UPDATE Prescricao p SET p.statusTratamento = :statusTratamento , p.descStatusTratamento = :descStatusTratamento WHERE p.id = :id")
+	void updateStatusTratamentoAndDescStatusTratamento (Integer statusTratamento , String descStatusTratamento , Integer id);
 	
+	
+	//@Param(value = "id") Integer id, @Param(value = "status") Integer status
 
 }
