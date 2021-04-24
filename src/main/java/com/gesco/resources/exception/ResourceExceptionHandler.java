e8174ee71973f23e13b64886806e51b4701fe993
Guilherme Torato <guii.torato@gmail.com>
2021-04-24 18:52:33 -0300

    Adicionado mais alguns métodos dentro da classe ResourceExceptionHandler

--- src/main/java/com/gesco/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/gesco/resources/exception/ResourceExceptionHandler.java
@@ -1,16 +1,26 @@
 package com.gesco.resources.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.gesco.services.exceptions.ObjectNotFoundException;
+import com.gesco.services.exceptions.RecordNotFoundException;
 
+@SuppressWarnings({"unchecked","rawtypes"})
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
@@ -19,5 +29,23 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	  @ExceptionHandler(RecordNotFoundException.class)
+	    public final ResponseEntity<Object> handleUserNotFoundException(RecordNotFoundException ex, WebRequest request) {
+	        List<String> details = new ArrayList<>();
+	        details.add(ex.getLocalizedMessage());
+	        ErrorResponse error = new ErrorResponse("Record Not Found", details);
+	        return new ResponseEntity(error, HttpStatus.NOT_FOUND);
+	    }
+	 
+	    @Override
+	    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+	        List<String> details = new ArrayList<>();
+	        for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+	            details.add(error.getDefaultMessage());
+	        }
+	        ErrorResponse error = new ErrorResponse("Validation Failed", details);
+	        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+	    }
 
 }
